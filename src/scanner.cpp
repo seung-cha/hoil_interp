21acf44ce4bf401ab8236794f1d2d72a4552def7
Seung Cha <seunghwancha5857@gmail.com>
2024-08-10 15:45:56 +1000

    implement char reading

--- src/scanner.cpp
+++ src/scanner.cpp
@@ -1,10 +1,10 @@
 #include "scanner.h"
 #include <iostream>
 #include <exception>
+#include <cassert>
 
 Scanner::Scanner(std::string source) : stream{source}
 {
-
 }
 
 Scanner::~Scanner()
@@ -15,16 +15,82 @@ Scanner::~Scanner()
     }
 }
 
+bool Scanner::Seek()
+{
+    _trimBlanks();
+    return _readChar();
+}
 
-char Scanner::ReadChar()
+void Scanner::ScanFile()
+{
+    while(Seek())
+    {
+        printf("%c", currentChar);
+    }
+}
+
+void Scanner::Match(char c)
+{
+    if(currentChar == c)
+    {
+        Seek();
+        charNo++;
+    }
+    else
+    {
+        // TODO:: Throw an exception here
+    }
+}
+
+
+bool Scanner::_readChar()
 {
     if(stream.is_open())
     {
-        return stream.get();
+        charNo++;
+        currentChar = stream.get();
+
+        if(currentChar == EOF)
+        {
+            return false;
+        }
+
+        return true;
     }
     else
     {
         // TODO: Throw exception here
     }
 
+    return currentChar;
 }
+
+char Scanner::_peek()
+{
+    return stream.peek();
+}
+
+void Scanner::_trimBlanks()
+{
+    while(true)
+    {
+        switch(_peek())
+        {
+            case ' ':
+            break;
+            case '\t':
+            charNo += BLANK_SPACE - 1; // _readChar() increments it by 1
+            break;
+            case '\n':
+            charNo = -1;    // _readChar() resets it to 0. 
+            lineNo++;
+            break;
+
+            default:
+            return;
+        }
+
+        _readChar();
+    }
+}
+
--- src/scanner.h
+++ src/scanner.h
@@ -4,14 +4,33 @@
 #include <string>
 #include <fstream>
 
+#define BLANK_SPACE 4
+
 class Scanner
 {
     public:
+    void ScanFile();
     Scanner(std::string source);
     ~Scanner();
-    char ReadChar();
 
     private:
+    char currentChar = 0;
+    int lineNo = 1, charNo = 0;
+
+    /**
+     * Get the next char that is not a whitespace.
+     * Internally calls _readChar() and _trimBlanks().
+     */
+    bool Seek();
+    void Match(char c);
+
+    void _trimBlanks();
+    char _peek();
+    /**
+     * Read a char and store it in currentChar.
+     * Return False if EOF is read.
+     */
+    bool _readChar();
     std::ifstream stream;
 
 
