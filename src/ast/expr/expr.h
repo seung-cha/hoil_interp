0ef9fee76b9279a80c4bfc2ee30db6216f3da9bf
Cha <seunghwancha5857@gmail.com>
2024-10-08 21:31:10 +1100

    add base expr and stmt class

---
+++ src/ast/expr/expr.h
@@ -0,0 +1,14 @@
+#ifndef AST_EXPR_H
+#define AST_EXPR_H
+
+#include "../ast.h"
+
+namespace ASTs
+{
+    class expr : public AST
+    {
+
+    };
+}
+
+#endif
---
+++ src/ast/stmt/stmt.h
@@ -0,0 +1,14 @@
+#ifndef AST_STMT_H
+#define AST_STMT_H
+
+#include "../ast.h"
+
+namespace ASTs
+{
+    class Stmt : public AST
+    {
+
+    };
+}
+
+#endif
