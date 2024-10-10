7ff03fbda569cf84e86a813e1f9cdf1faa777f1c
Cha <seunghwancha5857@gmail.com>
2024-10-10 23:59:44 +1100

    add param

---
+++ src/ast/list/param.h
@@ -0,0 +1,14 @@
+#ifndef AST_PARAM_H
+#define AST_PARAM_H
+
+#include "../ast.h"
+
+namespace ASTs
+{
+    class Param : public AST
+    {
+        // TODO: Var decl
+    };
+}
+
+#endif
---
+++ src/ast/list/param_list.h
@@ -0,0 +1,24 @@
+#ifndef AST_PARAM_LIST_H
+#define AST_PARAM_LIST_H
+
+#include "param.h"
+#include "list.h"
+#include <memory>
+
+namespace ASTs
+{
+    class ParamList : public List
+    {
+        public:
+        ParamList(Param *param, ParamList *next) : param{param}, next{next} 
+        {
+
+        }
+        
+        std::unique_ptr<Param> param;
+        std::unique_ptr<ParamList> next;
+
+    };
+}
+
+#endif
