84c3772bf38e7f06f56674cb95a40601ac83c2d5
Cha <seunghwancha5857@gmail.com>
2024-10-12 18:08:34 +1100

    update VarDeclBlock.h

--- src/ast/list/var_decl_block.h
+++ src/ast/list/var_decl_block.h
@@ -1,7 +1,7 @@
 #ifndef AST_VAR_DECL_BLOCK_H
 #define AST_VAR_DECL_BLOCK_H
 
-#include "../stmt/stmt.h"
+#include "../decl/decl.h"
 #include "block.h"
 #include "list.h"
 #include <memory>
@@ -10,7 +10,14 @@ namespace ASTs
 {
     class VarDeclBlock : public Block
     {
-        //TODO
+        public:
+        VarDeclBlock(Decl *decl, List *next) : decl{decl}, next{next}
+        {
+
+        }
+
+        std::unique_ptr<Decl> decl;
+        std::unique_ptr<List> next;
     };
 }
 
