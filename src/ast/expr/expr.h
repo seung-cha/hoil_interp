a12087723a48f65063467ec902fa7605f7dcfcf2
Cha <seunghwancha5857@gmail.com>
2024-10-20 19:40:15 +1100

    add type to expr

--- src/ast/expr/expr.h
+++ src/ast/expr/expr.h
@@ -2,12 +2,15 @@
 #define AST_EXPR_H
 
 #include "../ast.h"
+#include "../type/type.h"
+#include <memory>
 
 namespace ASTs
 {
     class Expr : public AST
     {
-
+        public:
+        std::unique_ptr<Type> type;
     };
 }
 
