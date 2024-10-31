b1a1a1ebf3d6b93fd5b0fd4c9ab105b76d9082d4
Seung Cha <seunghwancha5857@gmail.com>
2024-11-01 00:53:43 +1100

    add codegen stub class

---
+++ src/codegen.cpp
@@ -0,0 +1,306 @@
+#include "codegen.h"
+
+using namespace ASTs;
+
+CodeGen::CodeGen(Program *program, std::vector<std::unique_ptr<ASTs::FuncDecl>>& reserve) : program{program}, reserve{reserve}
+{
+
+}
+
+
+// Decls
+AST *CodeGen::VisitFuncDecl(FuncDecl *decl, AST *obj)
+{
+    return nullptr;
+}
+
+AST *CodeGen::VisitFuncDeclList(FuncDeclList *list, AST *obj)
+{
+    return nullptr;
+}
+
+AST *CodeGen::VisitVarDecl(VarDecl *decl, AST *obj)
+{
+    return nullptr;
+}
+
+AST *CodeGen::VisitVarDeclExpr(VarDeclExpr *expr, AST *obj)
+{
+    return nullptr;
+}
+
+// Exprs
+AST *CodeGen::VisitAssignExpr(AssignExpr *expr, AST *obj)
+{
+    return nullptr;
+}
+
+AST *CodeGen::VisitBinaryExpr(BinaryExpr *expr, AST *obj)
+{
+    return nullptr;
+}
+
+AST *CodeGen::VisitBoolExpr(BoolExpr *expr, AST *obj)
+{
+    return nullptr;
+}
+
+AST *CodeGen::VisitEmptyExpr(EmptyExpr *expr, AST *obj)
+{
+    return nullptr;
+}
+
+AST *CodeGen::VisitErrorExpr(ErrorExpr *expr, AST *obj)
+{
+    return nullptr;
+}
+
+AST *CodeGen::VisitFuncCallExpr(FunctionCallExpr *expr, AST *obj)
+{
+    return nullptr;
+}
+
+AST *CodeGen::VisitIntExpr(IntExpr *expr, AST *obj)
+{
+    return nullptr;
+}
+
+AST *CodeGen::VisitPostUnaryExpr(PostUnaryExpr *expr, AST *obj)
+{
+    return nullptr;
+}
+
+AST *CodeGen::VisitPreUnaryExpr(PreUnaryExpr *expr, AST *obj)
+{
+    return nullptr;
+}
+
+AST *CodeGen::VisitRealExpr(RealExpr *expr, AST *obj)
+{
+    return nullptr;
+}
+
+AST *CodeGen::VisitStringExpr(StringExpr *expr, AST *obj)
+{
+    return nullptr;
+}
+
+AST *CodeGen::VisitVarExpr(VariableExpr *expr, AST *obj)
+{
+    return nullptr;
+}
+
+AST *CodeGen::VisitBreakStmt(BreakStmt *stmt, AST *obj)
+{
+    return nullptr;
+}
+
+AST *CodeGen::VisitContinueStmt(ContinueStmt *stmt, AST *obj)
+{
+    return nullptr;
+}
+
+AST *CodeGen::VisitReturnStmt(ReturnStmt *stmt, AST *obj)
+{
+    return nullptr;
+}
+
+AST *CodeGen::VisitCompoundStmt(CompoundStmt *stmt, AST *obj)
+{
+    return nullptr;
+}
+
+AST *CodeGen::VisitWhileStmt(WhileStmt *stmt, AST *obj)
+{
+    return nullptr;
+}
+
+AST *CodeGen::VisitForStmt(ForStmt *stmt, AST *obj)
+{
+    return nullptr;    
+}
+
+AST *CodeGen::VisitDoWhileStmt(DoWhileStmt *stmt, AST *obj)
+{
+    return nullptr;
+}
+
+AST *CodeGen::VisitIfStmt(IfStmt *stmt, AST *obj)
+{
+    return nullptr;
+}
+
+AST *CodeGen::VisitElifStmt(ElifStmt *stmt, AST *obj)
+{
+    return nullptr;
+}
+
+AST *CodeGen::VisitEmptyStmt(EmptyStmt *stmt, AST *obj)
+{
+    return nullptr;
+}
+
+AST *CodeGen::VisitErrorStmt(ErrorStmt *stmt, AST *obj)
+{
+    return nullptr;
+}
+
+AST *CodeGen::VisitExprStmt(ExprStmt *stmt, AST *obj)
+{
+    return nullptr;
+}
+
+// Function args
+AST *CodeGen::VisitArgList(ArgList *list, AST *obj)
+{
+    return nullptr;
+}
+
+AST *CodeGen::VisitArg(Arg *arg, AST *obj)
+{
+    return nullptr;
+}
+
+AST *CodeGen::VisitBlockItemList(BlockItemList *list, AST *obj)
+{
+    return nullptr;
+}
+
+AST *CodeGen::VisitElifList(ElifList *list, AST *obj)
+{
+    return nullptr;
+}
+
+AST *CodeGen::VisitEmptyArgList(EmptyArgList *list, AST *obj)
+{
+    return nullptr;
+}
+
+AST *CodeGen::VisitEmptyBlockList(EmptyBlockList *list, AST *obj)
+{
+    return nullptr;
+}
+
+AST *CodeGen::VisitEmptyElifList(EmptyElifList *list, AST *obj)
+{
+    return nullptr;
+}
+
+AST *CodeGen::VisitEmptyVarDeclList(EmptyVarDeclList *list, AST *obj)
+{
+    return nullptr;
+}
+
+AST *CodeGen::VisitEmptyFuncDeclList(EmptyFuncDeclList *list, AST *obj)
+{
+    return nullptr;
+}
+
+AST *CodeGen::VisitEmptyParamList(EmptyParamList *list, AST *obj)
+{
+    return nullptr;
+}
+
+AST *CodeGen::VisitParamList(ParamList *list, AST *obj)
+{
+    return nullptr;
+}
+
+AST *CodeGen::VisitParam(Param *param, AST *obj)
+{
+    return nullptr;
+}
+
+AST *CodeGen::VisitStmtBlock(StmtBlock *block, AST *obj)
+{
+    return nullptr;
+}
+
+AST *CodeGen::VisitVarDeclListBlock(VarDeclListBlock *block, AST *obj)
+{
+    return nullptr;
+}
+
+AST *CodeGen::VisitVarDeclList(VarDeclList *list, AST *obj)
+{
+    return nullptr;
+}
+
+// Types
+// No need to do anything regarding type checking:
+// exprs and stmts will do instead.
+AST *CodeGen::VisitBoolType(BoolType *type, AST *obj) 
+{
+    return nullptr;
+}
+
+AST *CodeGen::VisitErrorType(ErrorType *type, AST *obj)
+{
+    return nullptr;
+}
+
+AST *CodeGen::VisitIntType(IntType *type, AST *obj)
+{
+    return nullptr;
+}
+
+AST *CodeGen::VisitRealType(RealType *type, AST *obj)
+{
+    return nullptr;
+}
+
+AST *CodeGen::VisitVoidType(VoidType *type, AST *obj)
+{
+    return nullptr;
+}
+
+AST *CodeGen::VisitStringType(StringType *type, AST *obj)
+{
+    return nullptr;
+}
+
+AST *CodeGen::VisitBoolLiteral(BoolLiteral *literal, AST *obj)
+{
+    return nullptr;
+}
+
+AST *CodeGen::VisitIntLiteral(IntLiteral *literal, AST *obj)
+{
+    return nullptr;
+}
+
+AST *CodeGen::VisitRealLiteral(RealLiteral *literal, AST *obj)
+{
+    return nullptr;
+}
+
+AST *CodeGen::VisitStringLiteral(StringLiteral *literal, AST *obj)
+{
+    return nullptr;
+}
+
+AST *CodeGen::VisitOperator(Operator *op, AST *obj)
+{
+    return nullptr;
+}
+
+AST *CodeGen::VisitVariable(Variable *variable, AST *obj)
+{
+    return nullptr;
+}
+
+AST *CodeGen::VisitIdentifier(Identifier *ident, AST *obj)
+{
+    return nullptr;
+}
+
+AST *CodeGen::VisitProgram(Program *program, AST *obj)
+{
+    return nullptr;
+}
+
+
+
+
+
+
---
+++ src/codegen.h
@@ -0,0 +1,97 @@
+#ifndef CODEGEN_H
+#define CODEGEN_H
+
+#include <vector>
+#include <memory>
+
+#include "ast/visitor.h"
+#include "AST.h"
+
+
+class CodeGen : public ASTs::Visitor
+{
+    public:
+    CodeGen(ASTs::Program *program, std::vector<std::unique_ptr<ASTs::FuncDecl>>& reserve);
+    // Decl
+    virtual ASTs::AST *VisitFuncDecl(ASTs::FuncDecl *decl, ASTs::AST *obj) override;
+    virtual ASTs::AST *VisitVarDecl(ASTs::VarDecl *decl, ASTs::AST *obj) override;
+
+    // Expr
+    virtual ASTs::AST *VisitAssignExpr(ASTs::AssignExpr *expr, ASTs::AST *obj) override;
+    virtual ASTs::AST *VisitBinaryExpr(ASTs::BinaryExpr *expr, ASTs::AST *obj) override;
+    virtual ASTs::AST *VisitBoolExpr(ASTs::BoolExpr *expr, ASTs::AST *obj) override;
+    virtual ASTs::AST *VisitEmptyExpr(ASTs::EmptyExpr *expr, ASTs::AST *obj) override;
+    virtual ASTs::AST *VisitErrorExpr(ASTs::ErrorExpr *expr, ASTs::AST *obj) override;
+    virtual ASTs::AST *VisitFuncCallExpr(ASTs::FunctionCallExpr *expr, ASTs::AST *obj) override;
+    virtual ASTs::AST *VisitIntExpr(ASTs::IntExpr *expr, ASTs::AST *obj) override;
+    virtual ASTs::AST *VisitPostUnaryExpr(ASTs::PostUnaryExpr *expr, ASTs::AST *obj) override;
+    virtual ASTs::AST *VisitPreUnaryExpr(ASTs::PreUnaryExpr *expr, ASTs::AST *obj) override;
+    virtual ASTs::AST *VisitRealExpr(ASTs::RealExpr *expr, ASTs::AST *obj) override;
+    virtual ASTs::AST *VisitStringExpr(ASTs::StringExpr *expr, ASTs::AST *obj) override;
+    virtual ASTs::AST *VisitVarDeclExpr(ASTs::VarDeclExpr *expr, ASTs::AST *obj) override;
+    virtual ASTs::AST *VisitVarExpr(ASTs::VariableExpr *expr, ASTs::AST *obj) override;
+
+    // Stmt
+    virtual ASTs::AST *VisitBreakStmt(ASTs::BreakStmt *stmt, ASTs::AST *obj) override;
+    virtual ASTs::AST *VisitCompoundStmt(ASTs::CompoundStmt *stmt, ASTs::AST *obj) override;
+    virtual ASTs::AST *VisitContinueStmt(ASTs::ContinueStmt *stmt, ASTs::AST *obj) override;
+    virtual ASTs::AST *VisitElifStmt(ASTs::ElifStmt *stmt, ASTs::AST *obj) override;
+    virtual ASTs::AST *VisitEmptyStmt(ASTs::EmptyStmt *stmt, ASTs::AST *obj) override;
+    virtual ASTs::AST *VisitErrorStmt(ASTs::ErrorStmt *stmt, ASTs::AST *obj) override;
+    virtual ASTs::AST *VisitExprStmt(ASTs::ExprStmt *stmt, ASTs::AST *obj) override;
+    virtual ASTs::AST *VisitIfStmt(ASTs::IfStmt *stmt, ASTs::AST *obj) override;
+    virtual ASTs::AST *VisitDoWhileStmt(ASTs::DoWhileStmt *stmt, ASTs::AST *obj) override;
+    virtual ASTs::AST *VisitForStmt(ASTs::ForStmt *stmt, ASTs::AST *obj) override;
+    virtual ASTs::AST *VisitWhileStmt(ASTs::WhileStmt* Stmt, ASTs::AST *obj) override;
+    virtual ASTs::AST *VisitReturnStmt(ASTs::ReturnStmt *stmt, ASTs::AST *obj) override;
+
+    // Lists
+    virtual ASTs::AST *VisitArgList(ASTs::ArgList *list, ASTs::AST *obj) override;
+    virtual ASTs::AST *VisitArg(ASTs::Arg *arg, ASTs::AST *obj) override;
+    virtual ASTs::AST *VisitBlockItemList(ASTs::BlockItemList *list, ASTs::AST *obj) override;
+    virtual ASTs::AST *VisitElifList(ASTs::ElifList *list, ASTs::AST *obj) override;
+    virtual ASTs::AST *VisitEmptyArgList(ASTs::EmptyArgList *list, ASTs::AST *obj) override;
+    virtual ASTs::AST *VisitEmptyBlockList(ASTs::EmptyBlockList *list, ASTs::AST *obj) override;
+    virtual ASTs::AST *VisitEmptyElifList(ASTs::EmptyElifList *list, ASTs::AST *obj) override;
+    virtual ASTs::AST *VisitEmptyFuncDeclList(ASTs::EmptyFuncDeclList *list, ASTs::AST *obj) override;
+    virtual ASTs::AST *VisitEmptyParamList(ASTs::EmptyParamList *list, ASTs::AST *obj) override;
+    virtual ASTs::AST *VisitEmptyVarDeclList(ASTs::EmptyVarDeclList *list, ASTs::AST *obj) override;
+    virtual ASTs::AST *VisitFuncDeclList(ASTs::FuncDeclList *list, ASTs::AST *obj) override;
+    virtual ASTs::AST *VisitParamList(ASTs::ParamList *list, ASTs::AST *obj) override;
+    virtual ASTs::AST *VisitParam(ASTs::Param *param, ASTs::AST *obj) override;
+    virtual ASTs::AST *VisitStmtBlock(ASTs::StmtBlock *block, ASTs::AST *obj) override;
+    virtual ASTs::AST *VisitVarDeclListBlock(ASTs::VarDeclListBlock *block, ASTs::AST *obj) override;
+    virtual ASTs::AST *VisitVarDeclList(ASTs::VarDeclList *list, ASTs::AST *obj) override;
+
+    // Types
+    virtual ASTs::AST *VisitBoolType(ASTs::BoolType *type, ASTs::AST *obj) override;
+    virtual ASTs::AST *VisitErrorType(ASTs::ErrorType *type, ASTs::AST *obj) override;
+    virtual ASTs::AST *VisitIntType(ASTs::IntType *type, ASTs::AST *obj) override;
+    virtual ASTs::AST *VisitRealType(ASTs::RealType *type, ASTs::AST *obj) override;
+    virtual ASTs::AST *VisitVoidType(ASTs::VoidType *type, ASTs::AST *obj) override;
+    virtual ASTs::AST *VisitStringType(ASTs::StringType *type, ASTs::AST *obj) override;
+
+    // Literals
+    virtual ASTs::AST *VisitBoolLiteral(ASTs::BoolLiteral *literal, ASTs::AST *obj) override;
+    virtual ASTs::AST *VisitIntLiteral(ASTs::IntLiteral *literal, ASTs::AST *obj) override;
+    virtual ASTs::AST *VisitRealLiteral(ASTs::RealLiteral *literal, ASTs::AST *obj) override;
+    virtual ASTs::AST *VisitStringLiteral(ASTs::StringLiteral *literal, ASTs::AST *obj) override;
+
+    // Operators
+    virtual ASTs::AST *VisitOperator(ASTs::Operator *op, ASTs::AST *obj) override;
+
+    // Variables
+    virtual ASTs::AST *VisitVariable(ASTs::Variable *variable, ASTs::AST *obj) override;
+    
+    // Ident
+    virtual ASTs::AST *VisitIdentifier(ASTs::Identifier *ident, ASTs::AST *obj) override;
+
+    virtual ASTs::AST *VisitProgram(ASTs::Program *program, ASTs::AST *obj) override;
+
+    private:
+    ASTs::Program *program;
+    std::vector<std::unique_ptr<ASTs::FuncDecl>>& reserve;
+
+};
+
+#endif
